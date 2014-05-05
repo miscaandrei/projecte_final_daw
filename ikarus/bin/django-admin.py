15e71e7f406c5d6345883010c0b27fd0220db5b1
Misca Andrei <miscaandrei@gmail.com>
2014-05-05 20:23:26 +0200

    PER FI FUNCIONA AMB EL DJANGO NON REL DELS C*****. Thank You.

---
+++ ikarus/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/andrei/Projectes/Ikarus/GitHub/projecte_final_daw/ikarus/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ ikarus_base/ikarus_base/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'ikarus_base.views.home', name='home'),
+    # url(r'^ikarus_base/', include('ikarus_base.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
